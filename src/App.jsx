5c6e47929b88290e85f304719ef7962a988fc790
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2024-11-04 20:41:30 +0330

    [feat] add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import MainLayout from "./pages/MainLayout";
 import HomePage from "./pages/HomaPage";
 import SkillsPage from "./pages/SkillsPage";
 import EducationPage from "./pages/EducationPage";
 import SportsPage from "./pages/SportsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout/>}>
-      <Route index element={<HomePage/>}></Route>
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />}></Route>
       <Route path="/skills" element={SkillsPage}></Route>
       <Route path="/sports" element={SportsPage}></Route>
       <Route path="/educations" element={EducationPage}></Route>
+      <Route path="/*" element={NotFoundPage}></Route>
     </Route>
   )
-)
-
+);
 
 function App() {
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 
 export default App;
